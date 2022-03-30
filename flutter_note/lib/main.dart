0c2e25cf09eb1412d2fc07d231557cf4de60704d
roiban1344 <81000453+roiban1344@users.noreply.github.com>
2022-03-31 00:38:56 +0900

    Make the MyApp class stateful

--- flutter_note/lib/main.dart
+++ flutter_note/lib/main.dart
@@ -4,17 +4,40 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var inputText = '';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Note'),
-      ),
-      body: Container(),
-    ));
+            appBar: AppBar(
+              title: const Text('Flutter Note'),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(children: [
+                const Spacer(),
+                TextField( //const除く？
+                  onChanged: (text) {
+                    setState(() { 
+                      inputText = text;
+                    });
+                  },
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                    onPressed: inputText.isEmpty ? null : () {}, //onTapではない？
+                    child: const Text('保存')),
+                const Spacer(),
+              ]),
+            )));
   }
 }
